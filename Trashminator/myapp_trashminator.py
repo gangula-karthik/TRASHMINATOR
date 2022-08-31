fd1409720797cee05c181b00c8ba602838392dbb
gangula-karthik <gkarthik923@gmail.com>
2022-08-31 23:26:49 +0800

    FINAL: IT WORKS 💪 💪 💪 💪

--- Trashminator/myapp_trashminator.py
+++ Trashminator/myapp_trashminator.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, request
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
-@app.route('/upload')
-def upload_file():
-   return render_template('upload.html')
+# @app.route('/upload')
+# def upload_file():
+#    return render_template('upload.html')
 	
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
-      f = request.files['file']
+      f = request.files['file_upload']
       f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+      return f'{f.filename} has been uploaded successfully'
 		
 if __name__ == '__main__':
    app.run(debug = True)
