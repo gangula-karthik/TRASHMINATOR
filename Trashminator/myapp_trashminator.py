99f2d8bee0a3f1f71eb1e0623fd03de9ba70c2b8
gangula-karthik <gkarthik923@gmail.com>
2022-09-01 14:39:33 +0800

    add: html file added when nothing is uploaded

--- Trashminator/myapp_trashminator.py
+++ Trashminator/myapp_trashminator.py
@@ -7,14 +7,20 @@ app = Flask(__name__)
 # @app.route('/upload')
 # def upload_file():
 #    return render_template('upload.html')
+
+@app.route('/nofile', methods=['GET', 'POST'])
+def nofile():
+   return render_template('nofile.html')
 	
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
       f = request.files['file_upload']
+      if f.filename == '':
+         return nofile()
       f.save(secure_filename(f.filename))
       file_path = os.path.abspath(f.filename)
-      return detectorTrash(file_path)
+      return file_path
 
 if __name__ == '__main__':
    app.run(debug = True, port=5000)
